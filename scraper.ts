02a7cfa3190c3f02434e5894edff611df0e257fd
Stefano Ramponi <stefano.ramponi.lav@gmail.com>
2023-02-25 11:54:23 +0100

    filled scraper more

--- scraper.ts
+++ scraper.ts
@@ -8,11 +8,29 @@ const BASE_URL = 'https://github.com/topics/playwright'
         // setup browser
         const browser = await playwright.chromium.launch({ headless: true})
         // set the context
-        const context = await browser.newContext()
+        const context = await browser.newContext({ userAgent: agent })
         const page = await context.newPage({ bypassCSP: true})
         await page.setDefaultTimeout(30000)
         await page.setViewportSize({ width: 800, height: 600})
         await page.goto(BASE_URL)
+
+        // get data
+        const repositories = await page.$$eval('article.border' , (repoCards) => {
+            return repoCards.map((card) => {
+                const [user, repo] = card.querySelectorAll('h3 a')
+
+                const formatText = (element) => element && element.innerText.trim()
+
+                return {
+                    user: formatText(user),
+                    repo: formatText(repo),
+                    url: repo.href,
+
+                }
+            })
+        })
+
+        // store data
 
         console.log(agent)
         // close browser
